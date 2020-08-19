f8c262895d3bf29b7afb61306d6c4cc497b0c9c0
kristyna cervenkova <kristyna275@gmail.com>
2020-08-19 13:54:53 +0200

    refactored code for enetring and seeing the names

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
-    'Testing infrastructure working!'
-  end
+    enable :sessions
+#   get '/' do
+#     'Testing infrastructure working!'
+#   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  get '/' do
+    erb :index
+  end 
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
 end
